8202d9e03ea11352281acc304747af84199fe4d7
Akhil Srinivas P <akhilsrinivasp@gmail.com>
2022-06-25 06:11:56 +0530

    Classes and Objects

---
+++ Week 2/L10 Classes and Objects/Date.java
@@ -0,0 +1,40 @@
+public class Date {
+    private int day, month, year;
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    public Date (Date d) {
+        this(d.day, d.month, d.year);
+    }
+    public Date(int day, int month)
+    {
+        this(day, month, 2022);
+    }
+
+    // accessor methods
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+    // mutator methods
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String toString() {
+        return day + "/" + month + "/" + year;
+    }
+}
---
+++ Week 2/L10 Classes and Objects/L10_ClassesandObjects.java
@@ -0,0 +1,6 @@
+public class L10_ClassesandObjects {
+    public static void main(String[] args) {
+        Date d = new Date(1, 1, 2000);
+        System.out.println(d);
+    }
+}
